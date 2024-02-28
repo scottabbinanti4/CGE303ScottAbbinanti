d924f0d5d2415de6a682842ac828a1e179f9b1bd
scottabbinanti4 <scottabbinanti1@gmail.com>
2024-02-28 12:56:10 -0600

    Panel

---
+++ CGEPrototype1/Assets/Scripts/TopDownPlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+    }
+
+}
